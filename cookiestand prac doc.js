5dd5193374806c33c6c4af9032d84664ef23dd9a
ccruz07 <cruz1706@gmail.com>
2016-07-21 13:38:43 -0700

    Updates to cookiestand prac doc. js adding store #2

--- cookiestand prac doc.js
+++ cookiestand prac doc.js
@@ -1,5 +1,5 @@
-
-
+//Class 7-DOM Manipulation methods & properties
+//STORE #1
     var salmonCookies = {
   location: 'Pioneer Square',
   minCustomer: 17,
@@ -31,7 +31,7 @@
       row.appendChild(cell);
       }
       var data = document.createElement('li');
-      var text = document.createTextNode('total: ' + this.totalCookies);
+      var text = document.createTextNode('Total: ' + this.totalCookies);
       data.appendChild(text);
       row.appendChild(data);
       return row;
@@ -48,10 +48,49 @@ table.appendChild(data);
 var cookieData = salmonCookies.getCookieOutput();
 table.appendChild(cookieData);
 }
-
 createTable();
 
-      /*for (var i =0; i < table.length; i++);/*
 
+//STORE #2
+var salmonCookies1 = {
+  location: 'Portland Airport',
+  minCustomer: 6,
+  maxCustomer: 24,
+  avgCookieSale: 1.2,
+  numberOfCookiesPerHour:[],
+  totalCookies: 0,
+  //FUNCTION #1
+  calculateCookies:function(){
+    var total = 0;
+    for(var i=0; i<8; i++){
+      var number = Math.floor(Math.random()*(salmonCookies.maxCustomer-salmonCookies.minCustomer+1) + salmonCookies.minCustomer);
+      this.numberOfCookiesPerHour.push(number);
+      total += number;
 
-/*tableCreate.();*/
+      console.log(this.numberOfCookiesPerHour);
+    }
+    this.totalCookies = total
+    return total;
+  },
+  //FUNCTION #2
+  getCookieOutput: function() {
+    var time =['10am','11am','12pm','1pm','2pm','3pm','4pm','5pm'];
+    var row =document.createElement('ul');
+    for(i=0; i<this.numberOfCookiesPerHour; i++)
+  {
+    var cell = document.createElement('li');
+    var data = document.createTextNode(time[i] + ' ' +this.numberOfCookiesPerHour[i]);
+    cell.appendChild(data);
+    row.appendChild(cell);
+}
+
+    var data = document.createElement('li');
+    var text = document.createTextNode('total:' + this.totalCookies);
+    data.appendChild(text);
+    row.appendChild(data);
+
+    return row;
+  },
+
+}
+salmonCookies1.calculateCookies();
